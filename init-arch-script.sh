0255443f76dc38d58000488f7c4529cf2f47e738
zewebdevf4gs <112774130+zewebdevf4gs@users.noreply.github.com>
2022-09-22 14:05:06 -0500

    Update init-arch-script.sh

--- init-arch-script.sh
+++ init-arch-script.sh
@@ -1,16 +1,23 @@
+#Install needed packages
 sudo pacman -S piper xfce4-whiskermenu-plugin xcape ntfs-3g firefox gparted gnome-disk-utility baobab galculator p7zip psensor virt-manager qemu-desktop libvirt edk2-ovmf dnsmasq iptables-nft syncthing
+#Enable Super key, Syncthing & libvirt daemon
 xcape -e 'Super_L=Alt_L|F1'
 systemctl enable --now syncthing.service --user
 sudo systemctl enable --now libvirtd.service
+#Install yay
 git clone https://aur.archlinux.org/yay-bin.git
 cd yay-bin
 makepkg -si
 cd ..
+#Remove yay's source folder
 rm -rf ./yay-bin
+#Install needed AUR packages
 yay -S yandex-browser kotatogram-desktop-bin
 git config --global user.email "4130+zewebdevf4gs@users.noreply.github.com"
 git config --global user.name "zewebdevf4gs"
+#Get base configuration files for the system type from github
 git clone https://github.com/zewebdevf4gs/base-arch-sys-config.git
+#Open downloaded config folder and local .config folder to restore needed/wanted configuration files
 sudo thunar base-arch-sys-config &
 sudo thunar /home/admin/.config &
-fb.me/profile.php_?id=Mf6F9EWwFhrTTlT11yWKjJ8Gy9UEAZZizA1a0Y2277T4Ce
+#fb.me/profile.php_?id=Mf6F9EWwFhrTTlT11yWKjJ8Gy9UEAZZizA1a0Y2277T4Ce
