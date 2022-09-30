81526f4eea8008be813a7261486d85da9cc82d78
zewebdevf4gs <112774130+zewebdevf4gs@users.noreply.github.com>
2022-09-30 10:29:58 -0500

    Update init-arch-script.sh

--- init-arch-script.sh
+++ init-arch-script.sh
@@ -2,7 +2,7 @@
 sudo pacman -Syyu
 # Install desired packages, separated for readability
 sudo pacman -S pacman-contrib piper gftp xfce4-whiskermenu-plugin ffmpeg cdrkit xdg-user-dirs neo-matrix ttf-vlgothic obs-studio virt-viewer python-pip 
-sudo pacamn -S xcape ntfs-3g firefox vlc gparted gnome-disk-utility baobab galculator p7zip psensor 
+sudo pacman -S xcape ntfs-3g firefox vlc gparted gnome-disk-utility baobab galculator p7zip psensor 
 sudo pacman -S virt-manager qemu-desktop libvirt edk2-ovmf dnsmasq iptables-nft syncthing
 # Install all of VLCs optional dependencies to fix fucked video playback -no kwallet
 sudo pacman -S pcsclite aribb25 aribb24 projectm libgoom2 lirc sdl_image libtiger libkate zvbi lua52-socket libmicrodns protobuf ttf-dejavu smbclient libmtp vcdimager libgme libva-intel-driver libva-vdpau-driver libdc1394
@@ -11,9 +11,10 @@ xdg-user-dirs-update
 # Install demucs
 pip3 install --user -U demucs
 # python3 -m demucs -d cpu PATH_TO_AUDIO_FILE_1
-# Enable Super key, Syncthing & libvirt daemon
+# Enable Super key, Syncthing, TRIM & libvirt daemon
 xcape -e 'Super_L=Alt_L|F1'
 systemctl enable --now syncthing.service --user
+sudo systemctl enable --now fstrim.timer
 sudo systemctl enable --now libvirtd.service
 # Install yay
 git clone https://aur.archlinux.org/yay-bin.git
