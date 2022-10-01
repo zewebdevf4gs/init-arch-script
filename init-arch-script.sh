d950562c325524d6280a0f9579eb1f831ba46d8d
zewebdevf4gs <112774130+zewebdevf4gs@users.noreply.github.com>
2022-09-30 21:24:09 -0500

    Update init-arch-script.sh

--- init-arch-script.sh
+++ init-arch-script.sh
@@ -1,9 +1,9 @@
 # Update
 sudo pacman -Syyu
 # Install desired packages, separated for readability
-sudo pacman -S pacman-contrib piper wmctrl gftp xfce4-whiskermenu-plugin ffmpeg cdrkit xdg-user-dirs neo-matrix ttf-vlgothic obs-studio virt-viewer python-pip 
-sudo pacman -S xcape ntfs-3g firefox vlc gparted gnome-disk-utility baobab galculator p7zip psensor 
-sudo pacman -S virt-manager qemu-desktop libvirt edk2-ovmf dnsmasq iptables-nft syncthing
+sudo pacman -S pacman-contrib piper wmctrl gftp xfce4-whiskermenu-plugin ffmpeg cdrkit xdg-user-dirs neo-matrix ttf-vlgothic zip python-pip 
+sudo pacman -S xcape ntfs-3g firefox vlc gparted gnome-disk-utility baobab galculator p7zip psensor syncthing obs-studio 
+sudo pacman -S virt-manager qemu-desktop libvirt edk2-ovmf dnsmasq iptables-nft virt-viewer 
 # Install all of VLCs optional dependencies to fix fucked video playback -no kwallet
 sudo pacman -S pcsclite aribb25 aribb24 projectm libgoom2 lirc sdl_image libtiger libkate zvbi lua52-socket libmicrodns protobuf ttf-dejavu smbclient libmtp vcdimager libgme libva-intel-driver libva-vdpau-driver libdc1394
 # Create user folders
